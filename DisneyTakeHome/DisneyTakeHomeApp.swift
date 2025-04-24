6e2a879044fc5b3e205aaf1d50e39beb13ff1de4
Oliver Paray <omparay@gmail.com>
2025-04-24 12:14:23 -0700

    Initial Commit

---
+++ DisneyTakeHome/DisneyTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisneyTakeHomeApp.swift
+//  DisneyTakeHome
+//
+//  Created by Oliver Paray on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct DisneyTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DisneyTakeHomeTests/DisneyTakeHomeTests.swift
@@ -0,0 +1,17 @@
+//
+//  DisneyTakeHomeTests.swift
+//  DisneyTakeHomeTests
+//
+//  Created by Oliver Paray on 4/24/25.
+//
+
+import Testing
+@testable import DisneyTakeHome
+
+struct DisneyTakeHomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
